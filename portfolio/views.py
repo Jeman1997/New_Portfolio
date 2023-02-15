7d48bdf5d46bb2a5db2f5cd837faee467a46534f
Jeman1997 <mr.j06031997@gmail.com>
2023-02-15 20:54:21 +0530

    New Porfolio feb 15 20:54

---
+++ portfolio/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+djangoprojects=[
+                {'title':'Notes','image':'Notes/Notes1.png','tags':['#Python','#Django','#user_authentication','#user_password_reset'],'link':'https://web-production-ee01.up.railway.app/'},
+                {'title':'News','image':'News/News2.jpeg','tags':['#Python','#Django','#user_authentication','#user_password_reset','#personalized_results'],'link':'https://news-app-django-final-production.up.railway.app/'},
+                {'title':'Blog','image':'Blog/Blog1.jpeg','tags':['#Python','#Django','#user_authentication','#user_password_reset'],'link':'https://web-production-9a4f.up.railway.app/'},
+                ]
+pythonprojects=[
+                {'title':'Snake','image':'Snake/Snake.jpeg','tags':['#Python','#Tkinter'],'link':'https://github.com/Jeman1997/Snake-Game'},
+                {'title':'Pong','image':'Pong/Pong.jpeg','tags':['#Python','#Tkinter'],'link':'https://github.com/Jeman1997/Pong'},
+                ]
+webscraping   =[
+                {'title':'Spotify Top 100 Songs','image':'SpotifyScrape/spotifytop100.jpeg','tags':['#Python','#Tkinter','#WebScraping'],'link':''}
+                ]
+def home(request):
+    cont={'req':'All','proj':djangoprojects+pythonprojects+webscraping}
+    if request.method=='POST':
+        cont={'req':request.POST['button']}
+        btn =request.POST['button']
+        if btn=='All':
+            cont['proj']=djangoprojects+pythonprojects+webscraping
+        elif btn=='Django':
+            cont['proj']=djangoprojects
+        elif btn=='Python':
+            cont['proj']=pythonprojects
+        elif btn=='WebScraping':
+            cont['proj']=webscraping
+    return render(request,'home.html',cont)
